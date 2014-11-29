450fe1e59d6e9d8af1312254e54c6b4bd70f65d8
John Landis <jalandis@gmail.com>
2014-11-29 18:44:42 -0500

    OpenWeather Api class along with test cases.

---
+++ library/OpenWeather/NetworkRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OpenWeather;
+
+/**
+ * Weather Report Mapper.
+ *
+ * @category Library
+ * @package  OpenWeather
+ * @author   John Landis <jalandis@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/brendanfmartin/weather_tracker/blob/master/library/OpenWeather/NetworkRequest.php
+ */
+class NetworkRequest
+{
+
+
+    /**
+     * Wrapper for a Curl network request.
+     *
+     * @param String $url Address for network request
+     *
+     * @return String json result of network call
+     */
+    public static function request($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $requestResults = curl_exec($curl);
+        curl_close($curl);
+
+        return $requestResults;
+
+    }//end request()
+
+
+}//end class
+
+?>
