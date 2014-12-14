eea7817103bff7a735a1cfdf864d0b6188407a2a
John Landis <jalandis@gmail.com>
2014-12-14 11:05:33 -0500

    get and getCollection methods for ForecastsController

--- webservice/Controllers/ForecastsController.php
+++ webservice/Controllers/ForecastsController.php
@@ -5,7 +5,7 @@ namespace Controllers;
 use Mappers\WeatherReportDbMapper;
 
 /**
- * Forecast Reports Controller.
+ * Forecast Controller.
  *
  * @category WebService
  * @package  Controllers
@@ -31,14 +31,49 @@ class ForecastsController
 
 
     /**
-     * Forecast collection action.
+     * Weather forecast collection action.
      *
      * @return Void
      */
     public function getForecasts()
     {
+        $response = new \Response();
+        $forecasts  = WeatherReportDbMapper::getAllReports(true);
+
+        $response->setCode(200);
+        $response->setData($forecasts);
+        $response->setMessage('success');
+
+        return $response->toJson();
 
     }//end getForecasts()
+
+
+    /**
+     * Report get action.
+     *
+     * @param mixed $id Id for Weather Report record
+     *
+     * @return String json encoded response
+     */
+    public function getForecast($id)
+    {
+        $response = new \Response();
+        $forecast   = WeatherReportDbMapper::getReportById($id);
+
+        if ($forecast !== false) {
+            $response->setCode(200);
+            $response->setData($forecast);
+            $response->setMessage('success');
+        } else {
+            $response->setCode(404);
+            $response->setData(null);
+            $response->setMessage("failed to find weather forecast record with id: {$id}.");
+        }
+
+        return $response->toJson();
+
+    }//end getForecast()
 
 
 }//end class
