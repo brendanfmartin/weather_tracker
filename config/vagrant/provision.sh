7f237ce779b9210b621adbc6b1c71a66ca6bef70
John Landis <jalandis@gmail.com>
2014-12-20 19:22:52 -0500

    Initial setup for server.  Incomplete.

--- config/vagrant/provision.sh
+++ config/vagrant/provision.sh
@@ -6,7 +6,7 @@
 #  - Install apache
 #  - Setup configuration
 
-#apt-get -y update
+apt-get -y update
 sudo apt-get -y install postgresql postgresql-contrib php5-pgsql
 cp -f database/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
 service postgresql restart
@@ -16,6 +16,8 @@ apt-get -y install apache2
 apt-get -y install libapache2-mod-php5
 a2enmod php5
 
-echo "ServerName weather-tracker" | tee /etc/apache2/conf-available/fqdn.conf
+echo "ServerName weather-tracker.com" | tee /etc/apache2/conf-available/fqdn.conf
 a2enconf fqdn
+a2dissite weather-tracker.com
+
 service apache2 restart
